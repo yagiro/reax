b25f112e58d09ffb989c285f322d2e4ab738e980
Jagiro Elmodovar <jagiro@walla.com>
2018-03-31 19:35:41 +0300

    changed name to Reax - react redux starter

--- app/App.js
+++ app/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-require('./sass/index.scss');
+require('../sass/index.scss');
 
 class App extends Component {
     render() {
         return (
-            <div className="flex-row">
-                <div>
-                    <h1>React Starter</h1>
-                    <h4>Created with love by Yakir Rabinovich</h4>
-                </div>
+            <div className="flex-col">
+                <span className="title">Reax</span>
+                <span className="sub-title">React &amp; Redux Starter</span>
+                <span className="sub-title">Created with love by Yakir Rabinovich</span>
             </div>
         )
     }
