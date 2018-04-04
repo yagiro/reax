4111e3ad72cbee0124fada3b17900ee3121cf1d5
Jagiro Elmodovar <jagiro@walla.com>
2018-04-04 03:30:24 +0300

    encapsulate App into custom components

--- app/App.js
+++ app/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 require('./sass/index.scss');
+import store from './store';
 
 class App extends Component {
     render() {
+        let color = '#ce80c6';
         return (
             <div className="flex-col">
-                <div className="title" style={{ marginBottom: '-15px', color: '#ce80c6' }}>Reax</div>
-                <div className="sub-title" style={{ color: '#584a57' }}>React &amp; Redux Starter</div>
-                <div className="footer-text">
-                    Created with <span style={{ color: '#ce80c6', fontWeight: 'bold' }}>&lt;3</span> by Yagiro
-                </div>
+                <Title color={ color }>Reax</Title>
+                <SubTitle>React &amp; Redux Starter</SubTitle>
+                <CreditFooter color={ color }/>
             </div>
         )
     }
 }
+
+const Title = props => (
+    <div className="title" style={{ 
+        color: props.color || "#ce80c6",
+        marginBottom: '-15px',
+    }}>
+        { props.children }
+    </div>
+);
+
+const SubTitle = () => (
+    <div className="sub-title" style={{ color: '#584a57' }}>React &amp; Redux Starter</div>
+);
+
+const CreditFooter = props => (
+    <div className="footer-text">
+        Created with <span style={{
+            color: props.color || '#ce80c6',
+            fontWeight: 'bold',
+        }}>
+            &lt;3
+        </span> by Yagiro
+    </div>
+);
 
 export default App;
