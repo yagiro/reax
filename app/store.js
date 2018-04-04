c3192e47c2634384bdf77b1e89413a364809cbc8
Jagiro Elmodovar <jagiro@walla.com>
2018-04-05 00:14:00 +0300

    enable redux HMR

--- app/store.js
+++ app/store.js
@@ -1,6 +1,14 @@
 import { createStore } from 'redux';
+
 import rootReducer from './reducers/rootReducer';
 
 const store = createStore(rootReducer);
+
+if (module.hot) {
+    module.hot.accept('./reducers/rootReducer', () => {
+        const updatedReducer = require('./reducers/rootReducer').default;
+        store.replaceReducer(updatedReducer);
+    });
+}
 
 export default store;
